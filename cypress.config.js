df4320815fcec04a7f215fe8340b4c05396cbef4
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2022-07-02 00:40:59 -0400

    2da-clase-cookies

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://pokedexpokemon.netlify.app",
+    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
