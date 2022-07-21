801a5bf91672e0bb47c4802eba88516a96933160
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2022-07-20 23:01:36 -0400

    13/seguridad

--- cypress/e2e/seguridad.cy.js
+++ cypress/e2e/seguridad.cy.js
@@ -1,3 +1,5 @@
+let texto, text2;
+
 describe("Seguridad", function () {
   it("navegar diferentes domininios", function () {
     //esto va a fallar
@@ -6,15 +8,49 @@ describe("Seguridad", function () {
     cy.visit("https://pokedexpokemon.netlify.app");
   });
 
-  it.only("navego un dominio aqui", function () {
+  it("navego un dominio aqui", function () {
     cy.visit("https://todo-cypress-iota.vercel.app");
     cy.get("h1").invoke("text").as("titulo");
   });
-  it.only("navego otro dominio aqui", function () {
+  it("navego otro dominio aqui", function () {
     // si usamos el mismo dominio no va a fallar si queremos compartir informacion entre dominios
     cy.visit("https://todo-cypress-iota.vercel.app");
     //descoemntar este y el otro para que vean como falla
     // cy.visit("https://pokedexpokemon.netlify.app");
-    cy.log(this.titulo);
+  });
+
+  it.only("navego dos dominios en el mismo test", function () {
+    cy.visit("/");
+    cy.get("h1")
+      .first()
+      .invoke("text")
+      .then((text) => {
+        texto = text;
+      });
+    cy.origin(
+      "https://todo-cypress-iota.vercel.app",
+      { args: { texto } },
+      function ({ texto }) {
+        cy.visit("/");
+        // esto va a seguir fallando
+        // cy.log(this.titulo);
+        //se puede hacer con la variable pero se tiene que pasar como un argumento
+        cy.log(texto);
+
+        cy.get("h1")
+          .invoke("text")
+          .then((text) => {
+            //esto va a fallar
+            text2 = text;
+          });
+      }
+    );
+    //es importante vovler a la pagina original para que lo detecte sino nos mostrara un error
+    cy.visit("/");
+    // cy.wait(1000);
+    cy.get("h1").first().invoke("text").should("be.equal", "Bulbasaur");
+
+    //no sirve copartir asi
+    cy.log(text2);
   });
 });
